78cb5e416ef73333a2726107aded9f5a47e0db9e
Hary Zeldon <hary.zeldon@proton.me>
2025-10-03 12:08:22 +0200

    make test executable from any work dir
    Signed-off-by: Hary Zeldon <hary.zeldon@proton.me>

--- tests/sloggi.sh
+++ tests/sloggi.sh
@@ -10,8 +10,11 @@
 # XXX Output is not quite TAP, but should be. We could make a TAP mode for slog, or strip the
 # non-tap output in the log.
 
-LOG_PATH="./my.log"
-. ../slog.sh
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+readonly SCRIPT_DIR
+
+LOG_PATH="$SCRIPT_DIR/my.log"
+. "$SCRIPT_DIR/../slog.sh"
 
 log "ok 1 - This is regular log message... "
 
